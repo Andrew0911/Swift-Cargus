d179b49027100205f7636118e0f36ac30cbf7d2e
cristianh <cristianh@fancourier.ro>
2024-05-23 02:54:25 +0300

    add finalize page title

--- react/src/components/FinalizeAWB.jsx
+++ react/src/components/FinalizeAWB.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
+import SwiftCargusLogo from '../img/SwiftCargusLogo.png'
 import { useLocation, useNavigate } from 'react-router-dom';
 import FinalizePlusIcon from '../img/FinalizePlusIcon.png';
 import FinalizePrintIcon from '../img/FinalizePrintIcon.png';
@@ -25,41 +27,48 @@ function FinalizeAWB() {
     }
 
     return (
-        <div className='finalizePage-container'>
+        <>
+            <Helmet>
+                <title>AWB Finalize</title>
+                <link rel="icon" href={SwiftCargusLogo} type="image/png" />
+            </Helmet>
 
-            <div className='title'> 
-                The AWB &#xA0;  
-                <span>{awbNumber}</span> 
-                &#xA0; was sucessfully generated !
+            <div className='finalizePage-container'>
+
+                <div className='title'> 
+                    The AWB &#xA0;  
+                    <span>{awbNumber}</span> 
+                    &#xA0; was sucessfully generated !
+                </div>
+
+                <br/><br/><br/><br/><br/>
+
+                <div className='buttons-container'>
+                    
+                    <button onClick={() => redirect('AWB')}> 
+                        <img src={FinalizePlusIcon}></img> 
+                        <span style={{paddingLeft: '1vw'}}> Generate new AWB </span>
+                    </button>
+                    
+                    <button onClick={() => redirectToPrint()}>
+                        <img src={FinalizePrintIcon}></img> 
+                        <span style={{paddingLeft: '3vw'}}> Print AWB </span>
+                    </button>
+                    
+                    <button onClick={() => redirectToTracking()}> 
+                        <img src={FinalizeTrackingIcon}></img> 
+                        <span style={{paddingLeft: '3vw'}}> Track AWB </span> 
+                    </button>
+                    
+                    <button onClick={() => redirect('Dashboard')}> 
+                        <img src={FinalizeDashboardIcon}></img> 
+                        <span style={{paddingLeft: '3vw'}}> Dashboard </span>
+                    </button>
+                </div>
+            
+                <br/><br/><br/><br/><br/><br/><br/><br/>
             </div>
-
-            <br/><br/><br/><br/><br/>
-
-            <div className='buttons-container'>
-                
-                <button onClick={() => redirect('AWB')}> 
-                    <img src={FinalizePlusIcon}></img> 
-                    <span style={{paddingLeft: '1vw'}}> Generate new AWB </span>
-                </button>
-                
-                <button onClick={() => redirectToPrint()}>
-                    <img src={FinalizePrintIcon}></img> 
-                    <span style={{paddingLeft: '3vw'}}> Print AWB </span>
-                </button>
-                
-                <button onClick={() => redirectToTracking()}> 
-                    <img src={FinalizeTrackingIcon}></img> 
-                    <span style={{paddingLeft: '3vw'}}> Track AWB </span> 
-                </button>
-                
-                <button onClick={() => redirect('Dashboard')}> 
-                    <img src={FinalizeDashboardIcon}></img> 
-                    <span style={{paddingLeft: '3vw'}}> Dashboard </span>
-                </button>
-            </div>
-        
-            <br/><br/><br/><br/><br/><br/><br/><br/>
-        </div>
+        </>
   )
 }
 
